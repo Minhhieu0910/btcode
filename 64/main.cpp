f2fa5fe7d6edf697d26ba41fb7c0d1502fafa807
Minhhieu0910 <minhhieu091002@gmail.com>
2022-11-04 00:20:16 +0700

    first commit

---
+++ 64/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+//Giải phương trình bậc 1, 2, 4
+using namespace std;
+
+int main()
+{
+   float a,b,c,x;
+   cout<<"nhap he so a cua x^4"<<endl;
+   cin>>a;
+   cout<<"nhap he so a cua x^2"<<endl;
+   cin>>b;
+   cout<<"nhap he so tu do c"<<endl;
+   cin>>c;
+   if(x==0)
+   {
+       cout<<"phuong trinh vo nghiem"<<endl;
+   }
+   else if(x!=0)
+   {
+       if(a==0)
+       {
+
+       }
+   }
+    return 0;
+}
