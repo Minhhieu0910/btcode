cbe92beceaafb458811fe75b1ef5bcfaf4f6ed3f
Minhhieu0910 <117256437+Minhhieu0910@users.noreply.github.com>
2022-12-19 23:16:46 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,83 +1,44 @@
 #include <iostream>
-//Tìm dãy con toàn dương có tổng lớn nhất
+//Tạo mảng b chỉ chứa giá trị lẻ từ mảng a
 using namespace std;
 void nhapmang(int a[],int n)
 {
-   cout<<"nhap gia tri cua cac phan tu trong mang:"<<endl;
-   for(int i=0;i<n;i++)
-   {
-       cout<<"a["<<i<<"]= "<<endl;
-       cin>>a[i];
-   }
-}
-void xuatmangcon(int a[],int n)
-{
+    cout<<"nhap gia tri cho cac phan tu trong mang :"<<endl;
     for(int i=0;i<n;i++)
     {
-        for(int y=1;y<=n;y++)
-        {
-            for(int j=i;j<y;j++)
-            {
-                cout<<a[j]<<"\t";
-            }
-            cout<<endl;
-        }
-        cout<<endl;
+        cout<<"a["<<i<<"]="<<endl;
+        cin>>a[i];
     }
 }
-int duong(int b[],int n)
+void xuatmang(int b[],int nb)
 {
-    for(int i=0;i<n;i++)
+    cout<<"xuat mang b:"<<endl;
+    for(int j=0;j<nb;j++)
     {
-        if(b[i]<0)
-        {
-            return 0;
-        }
+        cout<<b[j]<<"\t";
     }
-    return 1;
 }
-int lonnhat(int b[],int n)
+void taomang(int a[],int n)
 {
-    int m=0;
-    for(int i=0;i<n;i++)
-    {
-        m=m+b[i];
-    }
-      return m;
-}
-void duongtong(int a[],int n)
-{
-    int x=0;
     int b[100];
-    for (int i=0;i<n;i++)
+    int nb=0;
+    for(int i=0;i<n;i++)
     {
-        for(int y=1;y<=n;y++)
+        if(a[i]%2!=0)
         {
-            int nb=0;
-            for(int j=i;j<y;j++)
-            {
-                b[nb]=a[j];
-                nb++;
-                if(duong(b,nb)==1)
-                {
-                  if(lonnhat(b,nb)>x)
-                  {
-                      x=lonnhat(b,nb);
-                  }
-                }
-            }
+            b[nb]=a[i];
+            nb++;
         }
     }
-    cout<<"day toan duong co gia tri lon nhat bang =  "<<x<<endl;
+    xuatmang(b,nb);
 }
 int main()
 {
     int a[100];
     int n;
-    cout<<"nhap vao so phan tu trong mang "<<endl;
+    cout<<"nhap so luong phan tu trong mang a:"<<endl;
     cin>>n;
     nhapmang(a,n);
-    xuatmangcon(a,n);
-    duongtong(a,n);
+    taomang(a,n);
     return 0;
 }
