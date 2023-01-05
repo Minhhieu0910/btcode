8548c2a5d9b961e6a90710ee9e7ae412b3dd0a71
Minhhieu0910 <117256437+Minhhieu0910@users.noreply.github.com>
2023-01-06 06:56:21 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+// Viết hàm nhập, xuất ma trận vuông các số nguyên
+using namespace std;
+#define Max 100
+void nhapmatranvuong(int a[][Max],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cout<<"a["<<i<<"]["<<j<<"]= "<<endl;
+            cin>>a[i][j];
+        }
+    }
+}
+void xuatmatranvuong(int a[][Max],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cout<<a[i][j]<<"\t";
+        }
+        cout<<endl;
+    }
+}
+int main()
+{
+   int a[Max][Max];
+   int n;
+   cout<<"nhap so cot va dong cua ma tran vuong"<<endl;
+   cin>>n;
+   nhapmatranvuong(a,n);
+   xuatmatranvuong(a,n);
+
+    return 0;
+}
