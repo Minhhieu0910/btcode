6c7beea2de1c796c5d076d5d1b7b570c4b86e42f
Minhhieu0910 <117256437+Minhhieu0910@users.noreply.github.com>
2023-01-17 21:54:04 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,76 +1,111 @@
 #include <iostream>
-#include <stdio.h>
-#include <conio.h>
-/* Bài 615: Hãy khai báo kiểu dữ liệu để biểu diễn thông tin của một thí sinh (THISINH). Biết rằng một thí sinh gồm những thành phần sau:
+/* Bài 612: Hãy khai báo kiểu dữ liệu để biểu diễn thông tin của một cầu thủ. Biết rằng một cầu thủ gồm những thành phần sau:
 
-Mã thí sinh: chuỗi tối đa 5 ký tự
-Họ tên thí sinh: chuỗi tối đa 30 ký tự
-Điểm toán: kiểu số thực
-Điểm lý: kiểu số thực
-Điểm hóa: kiểu số thực
-Điểm tổng cộng: kiểu số thực
+Mã cầu thủ: chuỗi tối đa 10 ký tự
+Tên cầu thủ: chuỗi tối đa 30 ký tự
+Ngày sinh: kiểu dữ liệu ngày
 Sau đó viết hàm nhập, xuất cho kiểu dữ liệu này */
 
 using namespace std;
-struct thisinh
+struct ngay
 {
-    char mathisinh[5];
-    char hotenthisinh[30];
-    float diemtoan,diemly,diemhoa,diemtongcong;
+    int ng,th,nm;
 };
-void nhapthongtin(thisinh &a)
+struct thongtincauthu
 {
-    fflush(stdin);
-    cout<<"nhap ma thi sinh: "<<endl;
-    gets(a.mathisinh);
-    fflush(stdin);
-    cout<<"nhap ho ten thi sinh: "<<endl;
-    gets(a.hotenthisinh);
-    do
+    char macauthu[10];
+    char tencauthu[30];
+};
+int kiemtranamnhuan(ngay a)
+{
+    if(a.nm%4==0 && a.nm%100==0  ||(a.nm%400==0))
     {
-        cout<<"nhap vao diem toan: "<<endl;
-        cin>>a.diemtoan;
-        if(a.diemtoan<0 || a.diemtoan>10)
-        {
-            cout<<"vui long nhap diem lai!"<<endl;
-        }
-    }while (a.diemtoan<0 || a.diemtoan>10);
-    do
+        return 1;
+    }
+    else
     {
-        cout<<"nhap vao diem ly: "<<endl;
-        cin>>a.diemly;
-        if(a.diemly<0 || a.diemly>10)
-        {
-            cout<<"vui long nhap diem lai!"<<endl;
-        }
-    }while(a.diemly<0 || a.diemly>10);
-    do
-    {
-        cout<<"nhap vao diem hoa: "<<endl;
-        cin>>a.diemhoa;
-        if(a.diemhoa<0 || a.diemhoa>10)
-        {
-            cout<<"vui long nhap diem lai!"<<endl;
-        }
-    }while(a.diemhoa<0 || a.diemhoa>10);
+        return 0;
+    }
 
-        a.diemtongcong=(a.diemtoan + a.diemhoa + a.diemly)/3;
 }
-void xuatthongtin(thisinh a)
+int ngaytrongthang(ngay a)
 {
-    cout<<"Ma thi sinh: "<<a.mathisinh<<endl;
-    cout<<"Ho va ten thi sinh: "<<a.hotenthisinh<<endl;
-    cout<<"Diem toan: "<<a.diemtoan<<endl;
-    cout<<"Diem ly: "<<a.diemly<<endl;
-    cout<<"Diem hoa: "<<a.diemhoa<<endl;
-    cout<<"Diem Tong Cong: "<<a.diemtongcong<<endl;
+    int songay;
+    switch(a.th)
+    {
+
+         case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+        songay = 31;
+        break;
+        case 4: case 6: case 9 : case 11:
+        songay = 30;
+        break;
+        case 2:
+        int check = kiemtranamnhuan(a);
+        if(check == 1)
+        {
+           songay = 29;
+        }
+        else
+        {
+            songay = 28;
+        }
+
+    }
+    return songay;
+}
+void nhapngaythangnam(ngay &a)
+{
+    do
+    {
+        cout<<"nhap vao nam: "<<endl;
+        cin>>a.nm;
+        if(a.nm<1 )
+        {
+            cout<<"vui long nhap lai nam !"<<endl;
+        }
+    }while (a.nm<1);
+
+    do
+    {
+         cout<<"nhap vao ngay: "<<endl;
+         cin>>a.ng;
+         if(a.ng<1 || a.ng>ngaytrongthang(a))
+         {
+             cout<<"vui long nhap lai !";
+         }
+    }while(a.ng<1 || a.ng>ngaytrongthang(a));
+
+    do
+    {
+        cout<<"nhap vao thang: "<<endl;
+        cin>>a.th;
+        if(a.th<1 || a.th>12)
+        {
+            cout<<"vui long nhap lai thang !"<<endl;
+        }
+    }while(a.th<1 || a.th>12);
+
+}
+void nhapthongtin(thongtincauthu &ct)
+{
+    cout<<"nhap vao ma cau thu: "<<endl;
+    cin>>ct.macauthu;
+    cout<<"nhap vao ten cau thu: "<<endl;
+    cin>>ct.tencauthu;
+}
+void xuatthongtin(thongtincauthu ct, ngay a)
+{
+    cout<<"ma cau thu: "<<ct.macauthu<<endl;
+    cout<<"ten cau thu: "<<ct.tencauthu<<endl;
+    cout<<"ngay sinh: "<<a.ng<<"/"<<a.th<<"/"<<a.nm<<endl;
 }
 int main()
 {
-
-   thisinh a;
-   nhapthongtin(a);
-   xuatthongtin(a);
-   getch();
+    ngay a;
+    thongtincauthu ct;
+    nhapngaythangnam(a);
+    nhapthongtin(ct);
+    xuatthongtin(ct,a);
     return 0;
 }
