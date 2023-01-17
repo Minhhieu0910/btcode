f4ff8853b06eb683146ca567f8d0e609ec591c8c
Minhhieu0910 <117256437+Minhhieu0910@users.noreply.github.com>
2023-01-17 21:53:08 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,45 +1,76 @@
 #include <iostream>
-/* Bài 618: Hãy khai báo kiểu dữ liệu để biểu diễn thông tin của một lớp học (LOPHOC). Biết rằng một lớp học gồm những thành phần sau:
+#include <stdio.h>
+#include <conio.h>
+/* Bài 615: Hãy khai báo kiểu dữ liệu để biểu diễn thông tin của một thí sinh (THISINH). Biết rằng một thí sinh gồm những thành phần sau:
 
-Tên lớp: chuỗi tối đa 30 ký tự
-Sĩ số: kiểu số nguyên 2 byte
-Danh sách các học sinh trong lớp ( tối đa 50 học sinh)
+Mã thí sinh: chuỗi tối đa 5 ký tự
+Họ tên thí sinh: chuỗi tối đa 30 ký tự
+Điểm toán: kiểu số thực
+Điểm lý: kiểu số thực
+Điểm hóa: kiểu số thực
+Điểm tổng cộng: kiểu số thực
 Sau đó viết hàm nhập, xuất cho kiểu dữ liệu này */
+
 using namespace std;
-struct hocsinh
+struct thisinh
 {
-    char hoten[30];
+    char mathisinh[5];
+    char hotenthisinh[30];
+    float diemtoan,diemly,diemhoa,diemtongcong;
 };
-struct lophoc
+void nhapthongtin(thisinh &a)
 {
-    char tenlop[30];
-    int siso;
-    hocsinh danhsach[50];
-};
-void nhaphocsinh(hocsinh &hs)
-{
-     for(int i=0;i<lh.siso;i++)
-     {
-         cout<<"nhap vao ho va ten hoc sinh: "<<endl;
-         fflush(stdin);
-         gets(hs.hoten);
-     }
+    fflush(stdin);
+    cout<<"nhap ma thi sinh: "<<endl;
+    gets(a.mathisinh);
+    fflush(stdin);
+    cout<<"nhap ho ten thi sinh: "<<endl;
+    gets(a.hotenthisinh);
+    do
+    {
+        cout<<"nhap vao diem toan: "<<endl;
+        cin>>a.diemtoan;
+        if(a.diemtoan<0 || a.diemtoan>10)
+        {
+            cout<<"vui long nhap diem lai!"<<endl;
+        }
+    }while (a.diemtoan<0 || a.diemtoan>10);
+    do
+    {
+        cout<<"nhap vao diem ly: "<<endl;
+        cin>>a.diemly;
+        if(a.diemly<0 || a.diemly>10)
+        {
+            cout<<"vui long nhap diem lai!"<<endl;
+        }
+    }while(a.diemly<0 || a.diemly>10);
+    do
+    {
+        cout<<"nhap vao diem hoa: "<<endl;
+        cin>>a.diemhoa;
+        if(a.diemhoa<0 || a.diemhoa>10)
+        {
+            cout<<"vui long nhap diem lai!"<<endl;
+        }
+    }while(a.diemhoa<0 || a.diemhoa>10);
 
+        a.diemtongcong=(a.diemtoan + a.diemhoa + a.diemly)/3;
 }
-void nhapthongtin(lophoc &lh)
+void xuatthongtin(thisinh a)
 {
-    fflush(stdin);
-    cout<<"nhap vao ten lop: "<<endl;
-    gets(lh.tenlop);
-    cout<<"nhap vao si so cua lop: "<<endl;
-    fflush(stdin);
-    cin>>lh.siso
-    nhaphocsinh(lh.danhsach[i]);
-
+    cout<<"Ma thi sinh: "<<a.mathisinh<<endl;
+    cout<<"Ho va ten thi sinh: "<<a.hotenthisinh<<endl;
+    cout<<"Diem toan: "<<a.diemtoan<<endl;
+    cout<<"Diem ly: "<<a.diemly<<endl;
+    cout<<"Diem hoa: "<<a.diemhoa<<endl;
+    cout<<"Diem Tong Cong: "<<a.diemtongcong<<endl;
 }
 int main()
 {
-    lophoc lh;
-    nhapthongtin(lh);
+
+   thisinh a;
+   nhapthongtin(a);
+   xuatthongtin(a);
+   getch();
     return 0;
 }
